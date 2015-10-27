a5ba2fa836e68e8b87969100546832baacb54c38
Jason Marshall <jdmarshall@users.noreply.github.com>
2015-10-27 14:44:40 -0700

    Upgrading Ember.
    Again.

---
+++ ember-cli-build.js
@@ -0,0 +1,34 @@
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    // Add options here
+  });
+
+  // Use `app.import` to add additional libraries to the generated
+  // output files.
+  //
+  // If you need to use different assets in different
+  // environments, specify an object as the first parameter. That
+  // object's keys should be the environment name and the values
+  // should be the asset to use in that environment.
+  //
+  // If the library that you are including contains AMD or ES6
+  // modules that you would like to import into your application
+  // please specify an object with the list of modules as keys
+  // along with the exports of each module as its value.
+
+  app.import('bower_components/codemirror/lib/codemirror.css');
+  app.import('bower_components/codemirror/lib/codemirror.js');
+  app.import('bower_components/codemirror/addon/edit/continuelist.js');
+  app.import('bower_components/codemirror/mode/markdown/markdown.js');
+  app.import('bower_components/codemirror/mode/xml/xml.js');
+  app.import('bower_components/marked/lib/marked.js');
+
+  app.import('bower_components/jquery.markdownify/lib/jquery.markdownify.css');
+  app.import('bower_components/jquery.markdownify/lib/jquery.markdownify.js');
+
+
+  return app.toTree();
+};
