52e1eb5b613365a16b9cfd3765c51e1b419d05df
Jason Marshall <jdmarshall@users.noreply.github.com>
2015-10-27 23:11:47 -0700

    plant cleanup

--- app/models/plant.js
+++ app/models/plant.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     name: DS.attr('string', { defaultValue: 'Unnamed plant' }),
     description: DS.attr('string', { defaultValue: '' }),
-    kind: DS.belongsTo('group', { defaultValue: 'unknown'})
+    group: DS.belongsTo('group', { defaultValue: 'unknown'})
 });
