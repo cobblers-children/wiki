5db253a69dc439842de8c1946c46d49d63e01d5d
Jason Marshall <jdmarshall@users.noreply.github.com>
2015-10-27 20:48:04 -0700

    Start pulling in plant data from the map project.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
+import DS from 'ember-data';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
@@ -10,7 +11,22 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver: Resolver,
+    ApplicationSerializer: DS.RESTSerializer
+});
+
+Ember.Application.initializer({
+    name: 'preload-data',
+    initialize: function(container, application) {
+        var store = container.lookup('store:main');
+
+        application.deferReadiness();
+
+        Ember.$.getJSON('/plant_data.json').then(function(plant_json) {
+            store.pushPayload(plant_json);
+            application.advanceReadiness();
+        });
+    }
 });
 
 loadInitializers(App, config.modulePrefix);
