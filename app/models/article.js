f4804ffcbc9ec9631cd9fa13c9a9e17d6053dcbd
Jason Marshall <jdmarshall@users.noreply.github.com>
2015-11-01 23:33:36 -0800

    Adding an image to article

--- app/models/article.js
+++ app/models/article.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     name: DS.attr('string', { defaultValue: '' }),
     description: DS.attr('string', { defaultValue: '' }),
+    image: DS.attr('string'),
     body: DS.attr('string', { defaultValue: '' }),
     bodyLines: function() {
         return this.get('body').split(',').join('\n');
