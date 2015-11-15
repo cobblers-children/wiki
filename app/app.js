bf1191ed0bcb12eb6deb8dcdca491e07d71054ac
Jason Marshall <jdmarshall@users.noreply.github.com>
2015-11-14 22:28:10 -0800

    Stop preloading data, and clear some deprecation warnings in the REST adapters.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
-import DS from 'ember-data';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
@@ -11,23 +10,7 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver, 
-  ApplicationSerializer: DS.RESTSerializer
-});
-
-Ember.Application.initializer({
-    name: 'preload-data',
-    initialize: function(container, application) {
-        var store = container.lookup('store:main');
-
-        application.deferReadiness();
-
-        Ember.$.getJSON('/plant_data.json').then(function(plant_json) {
-            store.pushPayload(plant_json);
-
-            application.advanceReadiness();
-        });
-    }
+  Resolver: Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
