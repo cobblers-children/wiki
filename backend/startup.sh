fe36b738ee374a3d52acbf0e6e4ebce5025401f4
Jason Marshall <jdmarshall@users.noreply.github.com>
2016-02-07 22:41:20 -0800

    FIXED: #8  wait for postgres to start

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+if ! nc -z $POSTGRES_HOST 5432; then
+    echo "** Waiting for Postgres to start **"
+    WAIT=0
+    while ! nc -z $POSTGRES_HOST 5432; do
+        sleep 1
+        WAIT=$(($WAIT + 1))
+
+        if [ "$WAIT" -gt 15 ]; then
+            echo "Error: Postgres taking too long."
+            exit 1
+        fi
+    done
+fi
+
 node_modules/.bin/knex migrate:latest && ./server.js
