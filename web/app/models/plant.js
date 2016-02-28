a15dbde081887ebe87831aa5e0dca16bba98cc9c
Jason Marshall <jdmarshall@users.noreply.github.com>
2016-02-27 18:29:43 -0800

    Images for plants

--- web/app/models/plant.js
+++ web/app/models/plant.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     name: DS.attr('string', { defaultValue: 'Unnamed plant' }),
     description: DS.attr('string', { defaultValue: '' }),
+    image: DS.attr('string'),
     body: DS.attr('string', { defaultValue: '' }),
     group: DS.belongsTo('group', { defaultValue: 'unknown'})
 });
